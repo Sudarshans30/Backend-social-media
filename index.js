661a0ae5e55f7e718424089fea477daaedf05e58
sudarshans471-pqqgaF <sudarshans471@gmail.com>
2023-04-02 13:38:04 +1000

    added apiroutes

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
 const express = require('express');
-const db = require('./config/connection');
-
+const mongoose = require('mongoose');
 const routes = require('./routes');
-const cwd = process.cwd();
 
-
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const activity = cwd.includes('Backend-social-media') 
-? cwd.split ('/Backend-social-media/')[1]
-: cwd; 
+const activity = __dirname.includes('Backend-social-media')
+  ? __dirname.split('/Backend-social-media/')[1]
+  : __dirname;
+
+mongoose.connect('mongodb://localhost/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(routes);
 
-db.once('open', () => {
-    app.listen(PORT, () => {
-        console.log(`API server for ${activity} running on ${PORT}`)
-        });
-
-    }
-);
-
+mongoose.connection.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server for ${activity} running on ${PORT}`);
+  });
+});
